5515780e1e9992e47392013abbabdd830471a5cf
luljn <mbohlulajonathan4@gmail.com>
2024-05-29 12:11:18 +0200

    ReactiveForm added to the login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm!: FormGroup;
+  emailRegex!: RegExp; 
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router){}
+
+  ngOnInit(): void {
+      
+    this.loginForm = this.formBuilder.group({
+      mail: [null, Validators.required],
+      mdp: [null, Validators.required]
+    });
+  }
 }
