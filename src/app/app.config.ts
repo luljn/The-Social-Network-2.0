27a992444af3d642b695834a9804c4284ff46326
luljn <mbohlulajonathan4@gmail.com>
2024-05-30 22:12:55 +0200

    PostService added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(),
+    provideHttpClient(),
     { provide: LOCALE_ID, useValue: 'fr-FR' }
   ]
 };
