263a8dd2a19d9e12668d1fd8bd13677d3c8ab5c6
luljn <mbohlulajonathan4@gmail.com>
2024-06-05 22:27:20 +0200

    Chart.js dependencies added

--- src/app/statistiques/statistiques.component.ts
+++ src/app/statistiques/statistiques.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-statistiques',
@@ -7,6 +8,59 @@ import { Component } from '@angular/core';
   templateUrl: './statistiques.component.html',
   styleUrl: './statistiques.component.css'
 })
-export class StatistiquesComponent {
+export class StatistiquesComponent implements OnInit {
 
+  constructor(){
+
+    Chart.register(...registerables);
+  }
+
+  ngOnInit(): void {
+      
+    this.createCharts();
+  }
+
+  createCharts() {
+    const canvas = document.getElementById('followChart') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d');
+
+    if (ctx) {
+      new Chart(ctx, {
+        type: 'bar', // Exemple de type de graphique
+        data: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [{
+            label: 'Follows',
+            data: [12, 19, 3, 5, 2, 3, 7],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    } else {
+      console.error('Failed to get 2D context');
+    }
+  }
 }
